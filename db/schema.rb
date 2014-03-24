e10b56338200dcca87a93d4436db35324b3cbcef
Adam Kaplan <AdamHKaplan@gmail.com>
2014-03-24 12:04:27 -0700

    add gmaps4rails gem with map in facts index

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140322232159) do
+ActiveRecord::Schema.define(version: 20140324182643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,11 @@ ActiveRecord::Schema.define(version: 20140322232159) do
     t.integer  "geocode"
     t.string   "fee_code"
     t.integer  "duplicate_counts"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "facts", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
