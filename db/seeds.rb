3173cda3e7314372b5b3bbd91f1e6dee4f75bd19
Adam Kaplan <AdamHKaplan@gmail.com>
2014-03-25 20:16:45 -0700

    update README

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
 #we have to require the 'csv' library in order to play with the csv files, mmmkay?
-
+LiquorStore.destroy_all
+nl = 0
 CSV.foreach("db/abclicense.csv", headers: true) do |row|
 
-  LiquorStore.create(license_type:row["license_type"])
-   
+  LiquorStore.create(license_type:row["license_type"], county_id: row["premise_county"])
+  break
 end
 
 # This file should contain all the record creation needed to seed the database with its default values.
